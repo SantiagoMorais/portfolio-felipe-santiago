a8073255e97ed0074f58fed15e8b140b5235e040
Felipe Santiago Morais <fillikz@gmail.com>
2025-04-08 14:35:44 -0300

    feat(whatsApp): add utility for generating WhatsApp message link with greetings

---
+++ src/utils/whatsApp-link.ts
@@ -0,0 +1,28 @@
+import { env } from "@/env";
+
+const formatMessageForWeb = (message: string): string => {
+  return encodeURIComponent(message);
+};
+
+const greetingsHour = () => {
+  const date = new Date();
+  const hour = date.getHours();
+  const inTheMorning = hour >= 6 && hour < 12;
+  const inTheAfternoon = hour >= 12 && hour < 18;
+  const inTheNight = hour >= 18 && hour < 24;
+
+  switch (true) {
+    case inTheMorning:
+      return "Bom dia";
+    case inTheAfternoon:
+      return "Boa tarde";
+    case inTheNight:
+      return "Boa noite";
+    default:
+      return "Olá";
+  }
+};
+
+const defaultWhatsappMessage: string = `${greetingsHour()}, Felipe! Encontrei o seu contato em seu portfólio e gostaria de conversar`;
+
+export const whatsAppLink: string = `https://api.whatsApp.com/send?phone=${env.PHONE_NUMBER}&text=${formatMessageForWeb(defaultWhatsappMessage)}`;
