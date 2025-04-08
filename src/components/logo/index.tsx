3c88c2bb005f6baf3dac99c376c0aef8567cb6be
Felipe Santiago Morais <fillikz@gmail.com>
2025-04-08 13:09:14 -0300

    feat(Logo): add logo component

---
+++ src/components/logo/index.tsx
@@ -0,0 +1,21 @@
+import { MessageCircleCode } from "lucide-react";
+import { twJoin, twMerge } from "tailwind-merge";
+
+import { ILogoProps } from "@/core/interfaces/logo-props";
+
+export const Logo = ({ className, iconSize, padding }: ILogoProps) => (
+  <div className={twMerge("flex", padding)}>
+    <p
+      className={twJoin(
+        "text-foreground mt-2 text-2xl font-extrabold transition-colors duration-150 select-none first-letter:text-4xl",
+        className
+      )}
+    >
+      FS
+      <span className="bg-secondary ml-1 inline-block size-1.5 rounded-full" />
+    </p>
+    <MessageCircleCode
+      className={`transition-colors duration-150 ${twMerge("text-foreground size-12 duration-0", iconSize)}`}
+    />
+  </div>
+);
