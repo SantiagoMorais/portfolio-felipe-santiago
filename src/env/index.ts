95c81b548f49a8fc9576107de25ff409aaaebe8d
Felipe Santiago Morais <fillikz@gmail.com>
2025-04-18 13:17:55 -0300

    fix(env): reorder optional environment variables for better organization

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,14 +2,14 @@ import { z } from "zod";
 
 const envSchema = z.object({
   PHONE_NUMBER: z.coerce.string().default("5531996951033"),
-  EMAIL: z.string().email().optional(),
-  LINKEDIN_URL: z.string().url().optional(),
   GITHUB_PROFILE_URL: z.string().url(),
-  INSTAGRAM_URL: z.string().url().optional(),
   PROJECT_REPOSITORY_URL: z.string().url(),
   GITHUB_REPOSITORIES_URL: z.string().url(),
   DATABASE_URL: z.string().url(),
   DATABASE_ACCESS_TOKEN: z.string(),
+  EMAIL: z.string().email().optional(),
+  LINKEDIN_URL: z.string().url().optional(),
+  INSTAGRAM_URL: z.string().url().optional(),
 });
 
 const _env = envSchema.safeParse(process.env);
